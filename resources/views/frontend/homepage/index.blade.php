e56c796e05e30b08ee978bea70e27a788065b896
Anup Kasula <anup.kasula@elscript.com>
2024-03-02 17:27:29 +0545

    add pages

--- resources/views/frontend/homepage/index.blade.php
+++ resources/views/frontend/homepage/index.blade.php
@@ -10,10 +10,20 @@
             @include('frontend.homepage.brandslide')
             @include('frontend.homepage.popularcategory')
             @include('frontend.homepage.trendingproduct')
+        </div>
+        @include('frontend.homepage.parallax')
+        <div class="max-md:mx-4 my-4 mx-10 max-md:px-0 px-16">
             @include('frontend.homepage.bestselller')
             @include('frontend.homepage.newarrival')
+        </div>
+        @include('frontend.homepage.banner')
+        <div class="max-md:mx-4 my-4 mx-10 max-md:px-0 px-16">
             @include('frontend.homepage.blog')
             @include('frontend.homepage.faq')
+        </div>
+        <div class="-mb-14 ">
+
+            @include('frontend.homepage.secondparallax')
         </div>
 
 
