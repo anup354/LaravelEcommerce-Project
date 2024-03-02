ebd1102e058d0d1b7e08ab376c1aca4ac5ad1710
Anup Kasula <anup.kasula@elscript.com>
2024-03-02 17:26:12 +0545

    clean code

--- resources/views/frontend/_layout/menunav.blade.php
+++ resources/views/frontend/_layout/menunav.blade.php
@@ -11,36 +11,7 @@
                         <a class="nav-link transition  {{ request()->is('/') ? 'text-white underline' : 'text-gray-200 hover:text-white border-b border-b-transparent hover:border-b-2 hover:border-white ' }}"
                             href="{{ route('home') }}">HOME</a>
                     </li>
-                    {{-- <li>
-                   <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown"
-                   class="flex items-center justify-between nav-link text-gray-100 transition hover:text-[#ec1464]">
-                   CATEGORIES <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true"
-                       xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                           stroke-width="2" d="m1 1 4 4 4-4" />
-                   </svg>
-                      </button>
-                  <div id="mega-menu-dropdown" class="absolute z-50 hidden">
-                   <div
-                       class="p-4 pb-0 text-black md:pb-4 d  w-full ml-28 max-w-4xl shadow-lg  bg-white rounded-lg border">
-                       <div class="grid grid-cols-3 w-full gap-5 py-5">
-                           @foreach (getCategory() as $category)
-                               <a href=""
-                                   class="flex gap-2 hover:bg-gray-100 cursor-pointer p-3 rounded-md">
-                                   <div class=""><img
-                                           src="{{ asset('images/category/' . $category->image) }}"
-                                           class="w-24 " alt=""></div>
-                                   <div class="">
-                                       <div class="text-lg text-[#ec1464]">{{ $category->category_name }}</div>
-                                       <div class="text-sm text-gray-600 ">{{ $category->description }}</div>
-                                   </div>
-                               </a>
-                           @endforeach
-                       </div>
-                   </div>
 
-                 </div>
-                  </li> --}}
                     <li class="relative group">
                         <a href="
                         {{-- {{ route('about-us') }} --}}
@@ -67,7 +38,7 @@
                                 <div class="grid grid-cols-3 gap-6 w-[520px] ">
                                     @foreach (getCategories(0) as $category)
                                         <a href="
-                                        {{ route('getbycategory', ['category' => $category->slug . '?0da2qwz=' . $category->category_id])  }}
+                                        {{ route('getbycategory', ['category' => $category->slug . '?0da2qwz=' . $category->category_id]) }}
                                         "
                                             class="text-center cursor-pointer">
                                             <div class="flex flex-col items-center">
@@ -104,4 +75,3 @@
         </div>
     </nav>
 </div>
-
