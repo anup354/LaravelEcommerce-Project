2215e192b78f1c173dfbf8fff4e01ca6562a6734
Anup Kasula <anup.kasula@elscript.com>
2024-03-02 17:27:17 +0545

    add banner

---
+++ resources/views/frontend/homepage/banner.blade.php
@@ -0,0 +1,17 @@
+<div
+    class="relative font-[sans-serif] before:absolute before:w-full before:h-full before:inset-0 before:bg-black before:opacity-50 before:z-10">
+    <img src="https://readymadeui.com/cardImg.webp" alt="Banner Image"
+        class="absolute inset-0 w-full h-full object-cover" />
+    <div
+        class="min-h-[300px] relative z-50 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-white p-6">
+        <h2 class="sm:text-3xl text-2xl text-white  font-bold mb-6">Join the Digital Shopping <span
+                class="text-[#f15a28]">Revolution</span> , Experience the <span class="text-[#f15a28]">Difference</span> !
+        </h2>
+        <p class="text-lg text-center">Bringing the Best of <span class="text-[#f15a28] font-semibold">Brands</span> to Your Doorstep
+        </p>
+        <a href="javascript:void(0)"
+            class="mt-8 bg-transparent text-white text-base font-semibold py-2.5 px-6 border-2 border-white rounded hover:bg-white hover:text-black transition duration-300 ease-in-out">
+            Shop Now
+        </a>
+    </div>
+</div>
