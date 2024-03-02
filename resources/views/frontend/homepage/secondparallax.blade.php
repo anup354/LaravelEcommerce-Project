39e79a149b9f2e79d5a50fac3c6ca323c7b8b6b5
Anup Kasula <anup.kasula@elscript.com>
2024-03-02 17:27:48 +0545

    add seconf parallax

---
+++ resources/views/frontend/homepage/secondparallax.blade.php
@@ -0,0 +1,7 @@
+<section class="pt-40 pb-32 relative">
+    <div class="absolute w-full h-full top-0 left-0 bg-cover bg-center bg-no-repeat bg-fixed"
+        style="background-image:url({{asset("images/parallax.jpg")}})">
+    </div>
+    <div class="absolute w-full h-full top-0 left-0 bg-black/60"></div>
+    <h1 class="text-white text-3xl font-semibold display-2 text-center relative z-10">Seize success, shop <span class="text-[#f15a28]">now</span>, explore our curated <span class="text-[#f15a28]">offerings</span> , thrive <span class="text-[#f15a28]">forever</span>.</h1>
+</section>
